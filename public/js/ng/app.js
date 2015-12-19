981ff1bcdf25c109504c59f3c6c96fec59a911d2
jSheraden <josasheraden@gmail.com>
2015-12-18 20:50:38 -0700

    Update app.js

--- public/js/ng/app.js
+++ public/js/ng/app.js
@@ -1,16 +1,25 @@
-var app = angular.module( 'AngularBlogApp', [ 'ngRoute' ] );
+var app = angular.module('AngularBlogApp', ['ngRoute'])
 
-app.config( [ '$routeProvider', function( $routeProvider ) {
-    $routeProvider.when( '/', {
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/', {
         controller: 'MainCtrl',
         controllerAs: 'app',
         templateUrl: '/js/ng/partials/main.html'
-    } );
-    
-    $routeProvider.otherwise( { redirectTo: '/' } );
-} ] );
+    }).when('/joey', {
+        controller: 'JoeyCtrl',
+        controllerAs: 'app',
+        'templateUrl': '/js/ng/partials/main.html'
+    }).otherwise({
+        redirectTo: '/'
+    });
+}])
 
-app.controller( 'MainCtrl', [ '$scope', function( $scope ) {
+.controller('MainCtrl', ['$scope', function($scope) {
     $scope.title = 'Home';
     $scope.message = 'This is the home page.';
-} ] );
+}])
+
+.controller('JoeyCtrl', ['$scope', function($scope) {
+    $scope.title = 'Joey\'s Page';
+    $scope.message = 'Hey there, this is my page!';
+}]);
