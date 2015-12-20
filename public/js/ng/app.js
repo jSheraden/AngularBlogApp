981a295c0eec71a5c390871f0918496b850ff603
jSheraden <josasheraden@gmail.com>
2015-12-19 21:16:23 -0700

    Update app.js

--- public/js/ng/app.js
+++ public/js/ng/app.js
@@ -3,26 +3,26 @@ var app = angular.module('AngularBlogApp', ['ngRoute'])
 
 // Define routes.
 .config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/', {
-        controller: 'MainCtrl',
-        controllerAs: 'app',
-        templateUrl: '/js/ng/partials/main.html'
-    }).when('/joey', {
-        controller: 'JoeyCtrl',
-        controllerAs: 'app',
-        templateUrl: '/js/ng/partials/main.html'
-    }).otherwise({
-        redirectTo: '/'
-    });
+  $routeProvider.when('/', {
+    controller: 'MainCtrl',
+    controllerAs: 'app',
+    templateUrl: '/js/ng/partials/main.html'
+  }).when('/joey', {
+    controller: 'JoeyCtrl',
+    controllerAs: 'app',
+    templateUrl: '/js/ng/partials/main.html'
+  }).otherwise({
+    redirectTo: '/'
+  });
 }])
 
 // Define controllers.
 .controller('MainCtrl', ['$scope', function($scope) {
-    $scope.title = 'Home';
-    $scope.message = 'This is the home page.';
+  $scope.title = 'Home';
+  $scope.message = 'This is the home page.';
 }])
 
 .controller('JoeyCtrl', ['$scope', function($scope) {
-    $scope.title = 'Joey\'s Page';
-    $scope.message = 'Hey there, this is my page!';
+  $scope.title = 'Joey\'s Page';
+  $scope.message = 'Hey there, this is my page!';
 }]);
